fa732bf0c71a5879eb48f77b0dfc6dcfb188104e
epa <epa@localhost.localdomain>
2017-12-06 15:33:31 +0000

    Making start on runtest

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-for i in {1..50}
+
+#While loop to ensure parameter entered is not greater than 10
+go=false;
+while [[! $go]]
 do
-# do something here
+	if [ $1 -le 11 ]; then
+
+  	go=true;
+
+	fi
 done
+
+#Run load test in background with a pararmeter no longer than 10
+./loadtest $1 &
+
+
+
+#Linux for loop
+#for i in {1..50}
+#do
+# do something here
+#done
