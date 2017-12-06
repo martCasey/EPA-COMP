c374914ffc6b469416b53c6f867e17f28938c243
epa <epa@localhost.localdomain>
2017-12-06 15:45:22 +0000

    Improved runtest.sh

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -2,7 +2,7 @@
 
 #While loop to ensure parameter entered is not greater than 10
 go=false;
-while [[! $go]]
+while [[! $go ]]
 do
 	if [ $1 -le 11 ]; then
 
@@ -14,6 +14,17 @@ done
 #Run load test in background with a pararmeter no longer than 10
 ./loadtest $1 &
 
+#While loop to ensure number of users is no longer than 50
+go2=false;
+
+while [[! $go2 ]]
+do
+  	if [ $2 -le 51 ]; then
+
+        go=true;
+
+        fi
+done
 
 
 #Linux for loop
