9a05c7e249ba431764e8a9b4598f779027fc4240
epa <epa@localhost.localdomain>
2017-12-06 16:20:35 +0000

    Need to improve file

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -2,7 +2,7 @@
 
 #While loop to ensure parameter entered is not greater than 10
 go=false;
-while [[! $go ]]
+while [ $go = 'false' ]
 do
 	if [ $1 -le 11 ]; then
 
@@ -11,13 +11,10 @@ do
 	fi
 done
 
-#Run load test in background with a pararmeter no longer than 10
-./loadtest $1 &
-
 #While loop to ensure number of users is no longer than 50
 go2=false;
 
-while [[! $go2 ]]
+while [! $go2 ]
 do
   	if [ $2 -le 51 ]; then
 
@@ -26,6 +23,13 @@ do
         fi
 done
 
+#Run load test in background with a pararmeter no longer than 10
+./loadtest $1 &
+
+sleep $2
+
+#pkill will only be here temporarily
+pkill loadtest.C
 
 #Linux for loop
 #for i in {1..50}
