1c92dab2900643a35b19d41a046850fc1f88768c
epa <epa@localhost.localdomain>
2017-12-10 15:12:50 +0000

    Fixed transactions and runtest.sh

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-#get rid of while loops
+#Writes the headings of the 3 values to the file datafile.dat
+echo "CO" "i" "Idle" > results.dat
 #Linux for loop
-for i in {1..50};
+for i in {1..150};
 do
-#Run load test in background with a pararmeter no longer than 10
+#Run load test in background
 #loadtest runs forever
-#Value i is the number of users on the system, 10 is the amount of time each load test will run for
-./loadtest $i 10 &
+#Value i is the number of users on the system
+./loadtest $i &
 #Collect and output CPU Utilization
 #mpstat command runs for 10 seconds each, and generates 1 report every 10 seconds
-idle=`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-cpuUT=`mpstat -o JSON | jq '100 - .sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-	echo $cpuUT $i $idle >> datafile.dat
+idle=`mpstat 10 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+#Read from the file synthetic.dat to get the CO
+CO=`wc -l ./synthetic.dat`
+#Append the values to the file datafile.dat
+	echo $CO $i $idle >> results.dat
+#Kills loadtest
 pkill loadtest
 done
-
-#Run load test in background with a pararmeter no longer than 10
-#./loadtest $1 &
-
-#sleep $2
-
-#pkill will only be here temporarily
-#pkill loadtest.C
-
-#Linux for loop
-#for i in {1..50}
-#do
-#do something here
-#done
