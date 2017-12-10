4274daa5473a9c6f77574e4aa109c28a49361968
epa <epa@localhost.localdomain>
2017-12-10 20:23:49 +0000

    File name no longer stored in wc

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -3,7 +3,7 @@
 #Writes the headings of the 3 values to the file datafile.dat
 echo "CO" "i" "Idle" > results.dat
 #Linux for loop
-for i in {1..150};
+for i in {1..50};
 do
 #Run load test in background
 #loadtest runs forever
@@ -13,7 +13,8 @@ do
 #mpstat command runs for 10 seconds each, and generates 1 report every 10 seconds
 idle=`mpstat 10 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
 #Read from the file synthetic.dat to get the CO
-CO=`wc -l ./synthetic.dat`
+#The < ensures wc -l does not also print the name of the file
+CO=`wc -l < ./synthetic.dat`
 #Append the values to the file datafile.dat
 	echo $CO $i $idle >> results.dat
 #Kills loadtest
